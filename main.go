f73e49def04311784938c48915cc18007428c959
KenChang <chiayuan830410@gmail.com>
2022-05-29 10:59:17 +0800

    test code

--- main.go
+++ main.go
@@ -49,4 +49,33 @@ func main() {
 	fmt.Println(moment.Walk(moment.Board[6][7], JUMPLEFT, 4))
 	fmt.Println(moment.Action, "\n", display(moment.Board))
 
+	fmt.Println(moment.Walk(moment.Board[9][6], RIGHTUP, 0))
+	fmt.Println(moment.Action, "\n", display(moment.Board))
+
+	fmt.Println(moment.Walk(moment.Board[6][4], DOWN, 2))
+	fmt.Println(moment.Action, "\n", display(moment.Board))
+
+	fmt.Println(moment.Walk(moment.Board[9][2], RIGHTUP, 0))
+	fmt.Println(moment.Action, "\n", display(moment.Board))
+
+	fmt.Println(moment.Walk(moment.Board[8][4], LEFT, 1))
+	fmt.Println(moment.Action, "\n", display(moment.Board))
+
+	fmt.Println(moment.Walk(moment.Board[7][4], LEFTDOWN, 0))
+	fmt.Println(moment.Action, "\n", display(moment.Board))
+
+	fmt.Println(moment.Walk(moment.Board[6][2], UP, 1))
+	fmt.Println(moment.Action, "\n", display(moment.Board))
+
+	fmt.Println(moment.Walk(moment.Board[3][2], DOWN, 1))
+	fmt.Println(moment.Action, "\n", display(moment.Board))
+
+	fmt.Println(moment.Walk(moment.Board[5][2], UP, 1))
+	fmt.Println(moment.Action, "\n", display(moment.Board))
+
+	fmt.Println(moment.Walk(moment.Board[3][4], DOWN, 2))
+	fmt.Println(moment.Action, "\n", display(moment.Board))
+
+	fmt.Println(moment.Walk(moment.Board[7][6], LEFTLEFTUP, 1))
+	fmt.Println(moment.Action, "\n", display(moment.Board))
 }
